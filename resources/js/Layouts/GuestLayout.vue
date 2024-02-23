1e1bf04d9489b047120c2dfd5598eb21633ac947
vinvui <admin@vinvui.com>
2024-02-23 16:34:04 +0100

    Mise à jour du layout invité - Suppression des liens externes

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -77,7 +77,7 @@
         <div class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
             <div class="flex grow flex-col gap-y-5 overflow-y-auto  px-6">
                 <div class="flex h-16 shrink-0 items-center">
-                    <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=white" alt="Your Company" />
+                    Vincent Vuillemin
                 </div>
                 <nav class="flex flex-1 flex-col">
                     <ul role="list" class="flex flex-1 flex-col gap-y-7">
@@ -153,7 +153,6 @@ const navigation = ref([
     { name: 'à propos', href: '#about' },
     { name: 'compétences', href: '#skills' },
     { name: 'formations', href: '#formations' },
-    { name: 'liens externes', href: '#links' },
     { name: 'projets', href: '#projects' },
 ]);
 
