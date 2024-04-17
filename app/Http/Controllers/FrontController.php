dcd243c057db5ab56938e18b461c7c352bf816b7
vinvui <admin@vinvui.com>
2024-04-17 15:04:40 +0200

    Mise à jour de la méthode index dans le contrôleur FrontController.php

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -9,14 +9,15 @@ use App\Models\About;
 use App\Models\Skill;
 use App\Models\Project;
 use App\Models\Formation;
+use Inertia\Response;
 
 class FrontController extends Controller
 {
     /**
-     * Call the front page with the about and links
+     * Call the front page with all the data needed
      * @return \Inertia\Response
      */
-    public function index(): \Inertia\Response
+    public function index(): Response
     {
         $about = About::first();
         $links = Link::all();
@@ -24,7 +25,7 @@ class FrontController extends Controller
         $skills = Skill::orderBy('title')->get();
         $projects = Project::with(['skills' => function ($query) {
             $query->orderBy('title');
-        }])->orderBy('end_date')->get();
+        }])->orderBy('start_date', 'desc')->get();
         $formations = Formation::latest('year')->get();
 
         return Inertia::render('Front/Welcome', compact('about', 'links', 'user', 'skills', 'projects', 'formations'));
