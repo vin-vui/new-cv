bc87dd70bc8466c067320201a45b90d5c49e72e4
vinvui <admin@vinvui.com>
2024-04-18 08:40:50 +0200

    Mise à jour de la section Contact dans Contact.vue

--- resources/js/Pages/Front/Contact.vue
+++ resources/js/Pages/Front/Contact.vue
@@ -8,13 +8,13 @@
             </h4>
         </div>
         <a :href="`mailto:${email}`" :title="`Envoyer un email à ${who}`" :aria-label="`Envoyer un email à ${who}`" target="_blank">
-            <div
-                class="relative group p-4 cursor-pointer rounded-md transition-all duration-200 motion-reduce:transition-none hover:bg-amber-400/10 hover:shadow-[inset_0_1px_0_0_rgba(252,211,77,0.1)] hover:drop-shadow-lg hover:!opacity-100 group-hover/list:opacity-50">
+            <div class="relative group p-4 cursor-pointer rounded-md transition-all duration-200 motion-reduce:transition-none hover:bg-amber-400/10 hover:shadow-[inset_0_1px_0_0_rgba(252,211,77,0.1)] hover:drop-shadow-lg hover:!opacity-100 group-hover/list:opacity-50">
                 <p class="text-lg font-medium tracking-tight text-slate-200 sm:text-xl">Prêt à transformer tes idées en réalité ?</p>
                 <p class="text-slate-500">Contacte-moi dès aujourd'hui et discutons ensemble de ton projet !</p>
-                <p class="mt-2 text-sm leading-normal flex items-center gap-1">Tu peux <a :href="`mailto:${email}`" :title="`Envoyer un email à ${who}`" :aria-label="`Envoyer un email à ${who}`" target="_blank" class="text-yellow-500 text-base font-semibold flex items-center gap-1">m'envoyer un email <PaperAirplaneIcon class="size-5 -rotate-45 group-hover:-translate-y-1 group-hover:translate-x-1 transition-all divide-purple-200" /></a>
-                    <span class="text-xs text-slate-600"> — parce que soyons honnêtes, personne n'aime les formulaires ! 😉</span>
-                </p>
+                <div class="flex flex-col md:flex-row md:items-center gap-1">
+                    <div class="mt-2 text-base font-semibold leading-normal flex items-center gap-1">Tu peux <a :href="`mailto:${email}`" :title="`Envoyer un email à ${who}`" :aria-label="`Envoyer un email à ${who}`" target="_blank" class="text-yellow-500 flex items-center gap-1">m'envoyer un email <PaperAirplaneIcon class="size-5 -rotate-45 group-hover:-translate-y-1 group-hover:translate-x-1 transition-all divide-purple-200" /></a></div>
+                    <div class="text-xs text-slate-600"> — parce que soyons honnêtes, personne n'aime les formulaires ! 😉</div>
+                </div>
             </div>
         </a>
     </section>
